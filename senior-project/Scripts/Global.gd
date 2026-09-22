8c3f6722783ec22d4a1da7cf2f1e9255a737a6d4
Emerald6468 <139382641+Emerald6468@users.noreply.github.com>
2026-09-22 12:17:50 -0500

    githubtest2

--- senior-project/Scripts/Global.gd
+++ senior-project/Scripts/Global.gd
@@ -3,6 +3,6 @@ extends Node
 #Make sure to have the vars that need to reset in the fuction
 var InsideMenu = false
 
-#resets all globals
+#resets all global variables
 func reset_all():
 	InsideMenu = false
