648a63bd6ea464d90bb264570712700cab959db2
Emerald6468 <139382641+Emerald6468@users.noreply.github.com>
2026-09-24 12:35:53 -0500

    Coyote time!!

--- senior-project/Scripts/player.gd
+++ senior-project/Scripts/player.gd
@@ -10,6 +10,11 @@ var max_speed = 26.0
 var accel = 0.02
 var deccel = .5
 
+#Coyote time
+var was_on_ground = false
+var coyote_time_available = false
+var c_start = false
+var c_time = .5
 #Camera
 var mouse_sensitivity = 0.4
 @onready var head: Node3D = $Head
@@ -30,23 +35,44 @@ func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
 	pos.y = sin(time * BOB_FREQ) * BOB_AMP
 	return pos
-	
+
+func coyote_time():
+	if !c_start:
+		c_start = true
+		await get_tree().create_timer(c_time).timeout
+		coyote_time_available = false
+
+
 func _physics_process(delta: float) -> void:
 	#QUIT GAME COMMAND/CTRL X
 	if Input.is_action_just_pressed("DevQuit"):get_tree().quit()
 	
 	#controlls mouse
 	if !Global.InsideMenu: Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+#region Gravity and Jumping
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if is_on_floor(): 
+		coyote_time_available = true
+		was_on_ground = true
+		c_start = false
+	elif coyote_time_available and was_on_ground : 
+		coyote_time()
+		was_on_ground = false
+	#Normal Jump
+	if Input.is_action_just_pressed("Jump") and (is_on_floor() or coyote_time_available):
+		coyote_time_available = false
 		velocity.y = JUMP_VELOCITY
+	print(str(coyote_time_available))
+#endregion
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+#region Movement
 	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -56,10 +82,12 @@ func _physics_process(delta: float) -> void:
 			if !running.playing:running.play()
 			cur_speed = move_toward(cur_speed,max_speed,accel)
 	else:
+		running.stop()
+		cur_speed = move_toward(cur_speed,SPEED,deccel)
 		velocity.x = move_toward(velocity.x, 0, cur_speed)
 		velocity.z = move_toward(velocity.z, 0, cur_speed)
-	if !direction or !is_on_floor():
-		cur_speed = move_toward(cur_speed,SPEED,deccel)
+	if !is_on_floor():
+		cur_speed = move_toward(cur_speed,SPEED,deccel/10)
 		running.stop()
 	
 	#Head bob
@@ -67,6 +95,7 @@ func _physics_process(delta: float) -> void:
 	camera.transform.origin = _headbob(t_bob)
 		
 	move_and_slide()
+#endregion
 	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
