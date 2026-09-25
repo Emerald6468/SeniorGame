e1e83d847f9ea67a6d51874590f2d500ef79c1a6
Emerald6468 <139382641+Emerald6468@users.noreply.github.com>
2026-09-24 23:18:31 -0500

    almost done slide trust its just broken now but trust

--- senior-project/Scripts/player.gd
+++ senior-project/Scripts/player.gd
@@ -6,9 +6,9 @@ const JUMP_VELOCITY = 4.5
 
 #Flexible Speed
 var cur_speed
-var max_speed = 26.0
-var accel = 0.02
-var deccel = .5
+var max_speed = 20.0
+var accel = 0.04
+var deccel = .25
 
 #Coyote time
 var was_on_ground = false
@@ -17,10 +17,20 @@ var c_start = false
 var c_time = .5
 
 #Crouch/Slide
+@export var slide_curve:Curve
 var slide_deccel = .03
 var slide_threshold = 12.0
 var is_sliding = false
 var is_crouching = false
+var slide_start = false
+var set_speed:float
+var curve_x = 0.0
+
+var head_level:float
+var crouch_level:float
+var slide_level:float
+var crouch_diff = .25
+var slide_diff = .45
 
 
 #Camera
@@ -37,9 +47,13 @@ var t_bob:float = 0.0
 @onready var running: AudioStreamPlayer = $Running
 @onready var jump: AudioStreamPlayer = $Jump
 @onready var jump_land: AudioStreamPlayer = $JumpLand
+@onready var slide: AudioStreamPlayer = $Slide
 
 func _ready() -> void:
 	cur_speed = SPEED
+	head_level = $Head.position.y
+	crouch_level = head_level - crouch_diff
+	slide_level = head_level - slide_diff
 
 func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
@@ -52,6 +66,18 @@ func coyote_time():
 		await get_tree().create_timer(c_time).timeout
 		coyote_time_available = false
 
+func slide_lerp():
+	if slide_start:
+		slide_start = false
+	if cur_speed < set_speed +5.0: 
+		print("rise")
+		cur_speed = slide_curve.sample(curve_x)
+	if cur_speed >= slide_threshold: 
+		print("slow")
+		cur_speed = slide_curve.sample(curve_x)
+	if slide_curve.max_domain > curve_x: curve_x += .1
+	print("done")
+		
 
 func _physics_process(delta: float) -> void:
 	#QUIT GAME COMMAND/CTRL X
@@ -80,7 +106,7 @@ func _physics_process(delta: float) -> void:
 		coyote_time_available = false
 		jump.play()
 		velocity.y = JUMP_VELOCITY
-	print(str(coyote_time_available))
+	#print(str(coyote_time_available))
 #endregion
 
 	# Get the input direction and handle the movement/deceleration.
@@ -94,13 +120,26 @@ func _physics_process(delta: float) -> void:
 		if is_on_floor():
 			if !running.playing:running.play()
 			if !is_crouching and !is_sliding: cur_speed = move_toward(cur_speed,max_speed,accel)
-			if Input.is_action_just_pressed("Slide"):
+			if Input.is_action_pressed("Slide"):
 				if cur_speed >= slide_threshold: is_sliding = true
 				else: is_crouching = true
+			if Input.is_action_just_pressed("Slide"):
+				slide_start = true
 			else: is_sliding = false;is_crouching = false
-			
+			if is_sliding: 
+				print("slide")
+				slide_lerp()
+				$Head.position.y = move_toward(slide_level,.02,$Head.position.y)
+				if !slide.playing: slide.play()
+			elif is_crouching:
+				slide.stop()
+				$Head.position.y = move_toward(crouch_level,.05,$Head.position.y)
+			else: 
+				$Head.position.y = move_toward(head_level,.2,$Head.position.y)
+				slide.stop()
 	else:
 		running.stop()
+		slide.stop()
 		cur_speed = move_toward(cur_speed,SPEED,deccel)
 		velocity.x = move_toward(velocity.x, 0, cur_speed)
 		velocity.z = move_toward(velocity.z, 0, cur_speed)
@@ -109,7 +148,8 @@ func _physics_process(delta: float) -> void:
 		running.stop()
 	
 	#Head bob
-	t_bob += delta * velocity.length() * float(is_on_floor())
+	print(str(float(!is_sliding)))
+	t_bob += delta * velocity.length() * float(is_on_floor()) * float(!is_sliding)
 	camera.transform.origin = _headbob(t_bob)
 		
 	move_and_slide()
